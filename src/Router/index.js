cae8ae6cbd86d1ff3c47041ec6a22a9b4b4be080
vimal <selvamvimaldz1@gmail.com>
2024-02-20 22:14:56 +0530

    add:feat

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import userRoute from './UserRouter/User.js';
 import movieRoute from './MovieRouter/Movie.js'
-// import Forget from './Forget/Forget.js'
-// import Food from './Food/Food.js'
-// import cart from './Cart/Cart.js'
-// import order from './Orders/Order.js'
-const route=express();
-route.use('/',userRoute);
-route.use('/',movieRoute)
-// route.use('/',Food)
-// route.use('/',cart)
-// route.use('/',order)
+import Forget from './Forget/Forget.js';
+
+const route = express();
+route.use('/', userRoute);
+route.use('/', movieRoute)
+route.use('/forgetpass', Forget)
 export default route;
